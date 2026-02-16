ae4e00e849fb0fceac25eec24413c275933d7cd3
Maqi-x <smart.tech.yt.kontakt@gmail.com>
2026-02-16 08:42:02 +0100

    feat(pp): add basic preprocessor API declarations

--- include/el-pp/preproc.h
+++ include/el-pp/preproc.h
@@ -1,10 +1,28 @@
 #pragma once
 
+#include <el-util/dynarena.h>
+
 #include <el-lexer/tokbuf.h>
+#include <el-lexer/token.h>
+
+#include <el-pp/error.h>
 #include <el-pp/vars.h>
 
 typedef struct ElPreprocessor {
     ElPpVars vars;
+
     ElTokenBuf buf;
+    usize buf_offset;
+
+    ElDynArena arena;
+
+    ElPpErrorDetails last_err_details;
 } ElPreprocessor;
+
+ElPpErrorCode el_pp_init(ElPreprocessor* pp);
+ElPpErrorCode el_pp_destroy(ElPreprocessor* pp);
+
+ElPpErrorCode el_pp_reset(ElPreprocessor* pp);
+
+ElPpErrorCode el_pp_preprocess(ElPreprocessor* pp, ElToken in_tok, ElToken* out_tok);
 
