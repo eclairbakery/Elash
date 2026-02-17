596f4f2c77b92ef196b5c613cc2c34afc7d5cd96
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-02-17 08:39:53 +0100

    feat(srcdoc): add srcdoc API definitions and types

---
+++ src/el-srcdoc/srcdoc.c
@@ -0,0 +1,25 @@
+#include <el-srcdoc/srcdoc.h>
+
+// TODO
+ElSrcDocErrorCode el_srcdoc_init_empty(ElSourceDocument* srcdoc);
+ElSrcDocErrorCode el_srcdoc_init_from_str(ElSourceDocument* srcdoc, ElStringView sv);
+ElSrcDocErrorCode el_srcdoc_init_from_file(ElSourceDocument* srcdoc, const char* path);
+
+ElSrcDocErrorCode el_srcdoc_copy(const ElSourceDocument* src, ElSourceDocument* dst);
+void el_srcdoc_move(ElSourceDocument* src, ElSourceDocument* dst);
+ElSrcDocErrorCode el_srcdoc_copy_from_strbuf(ElSourceDocument* srcdoc, const ElStringBuf* buf);
+void el_srcdoc_move_from_strbuf(ElSourceDocument* srcdoc, ElStringBuf* buf);
+
+void el_srcdoc_destroy(ElSourceDocument* srcdoc);
+void el_srcdoc_clear(ElSourceDocument* srcdoc);
+
+ElSrcDocErrorCode el_srcdoc_append_token(ElSourceDocument* srcdoc, ElToken tok);
+ElSrcDocErrorCode el_srcdoc_append_str(ElSourceDocument* srcdoc, ElStringView sv);
+
+ElSrcDocErrorCode el_srcdoc_concat(const ElSourceDocument* src1,
+                                   const ElSourceDocument* src2,
+                                   ElSourceDocument* dst);
+
+ElStringView el_srcdoc_content(const ElSourceDocument* srcdoc);
+usize el_srcdoc_length(const ElSourceDocument* srcdoc);
+
