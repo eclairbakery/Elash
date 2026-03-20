2bbbd292a51ea32085a5bab6e8b5048b2d421522
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-03-20 14:41:07 +0100

    feat(pp/expstack): add expand state struct and associated definitions

---
+++ include/el-pp/header.h
@@ -0,0 +1,8 @@
+#include <el-defs/sv.h>
+
+typedef struct ElPpHeaderFile {
+    ElStringView path;
+    bool is_system;
+} ElPpHeaderFile;
+
+#define EL_PP_HEADER_FILE_NULL ((ElPpHeaderFile) { .path = EL_SV_NULL, .is_system = false })
