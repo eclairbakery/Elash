54c5af2091d7eb1bbbe117907a2d66ce2ee2085a
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-19 16:51:20 +0200

    feat(parser): improve error handling defs

--- include/el-parser/error.h
+++ include/el-parser/error.h
@@ -1,10 +1,19 @@
 #pragma once
 
+#include <el-lexer/token.h>
+
 typedef enum ElParserErrorCode {
-    EL_PARSER_ERR_OK,
+    EL_PARSER_ERR_OK = 0,
+    EL_PARSER_ERR_UNEXPECTED_TOKEN,
+    EL_PARSER_ERR_EXPECTED_TOKEN,
+    EL_PARSER_ERR_UNEXPECTED_EOF,
+    EL_PARSER_ERR_PP_ERROR,
 } ElParserErrorCode;
 
 typedef struct ElParserErrorDetails {
     ElParserErrorCode code;
-    // TODO
+    union {
+        ElToken token;
+        ElTokenType expected;
+    } data;
 } ElParserErrorDetails;
