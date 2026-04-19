e6da01664e6bd4cd7010d76a09cf77ecd6dc84d5
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-19 15:30:14 +0200

    feat(ast): update note definition

--- include/el-ast/node.h
+++ include/el-ast/node.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "bin.h"
+#include "unary.h"
 
 typedef enum ElAstNodeType {
     EL_AST_NODE_BINARY_EXPR,
@@ -8,4 +10,8 @@ typedef enum ElAstNodeType {
 
 typedef struct ElAstNode {
     ElAstNodeType type;
+    union {
+        ElAstBinExprNode binary;
+        ElAstUnaryExprNode unary;
+    } as;
 } ElAstNode;
