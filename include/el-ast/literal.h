473637bc8cad834a7f16e223067b0645a5b59974
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-19 16:49:18 +0200

    feat(ast): add literal nodes definitions

---
+++ include/el-ast/literal.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <el-lexer/token.h>
+
+typedef enum ElAstLiteralType {
+    EL_AST_LIT_INT,
+    EL_AST_LIT_FLOAT,
+    EL_AST_LIT_CHAR,
+    EL_AST_LIT_STRING,
+    EL_AST_LIT_BOOL,
+    EL_AST_LIT_NULL,
+} ElAstLiteralType;
+
+typedef struct ElAstIntLiteralNode {
+    uint64_t value;
+} ElAstIntLiteralNode;
+
+typedef struct ElAstFloatLiteralNode {
+    long double value;
+} ElAstFloatLiteralNode;
+
+typedef struct ElAstCharLiteralNode {
+    char value;
+} ElAstCharLiteralNode;
+
+typedef struct ElAstStringLiteralNode {
+    ElStringView value;
+} ElAstStringLiteralNode;
+
+typedef struct ElAstBoolLiteralNode {
+    bool value;
+} ElAstBoolLiteralNode;
+
+typedef struct ElAstLiteralNode {
+    ElAstLiteralType type;
+    union {
+        ElAstIntLiteralNode int_;
+        ElAstFloatLiteralNode float_;
+        ElAstCharLiteralNode char_;
+        ElAstStringLiteralNode str_;
+        ElAstBoolLiteralNode bool_;
+    } of;
+} ElAstLiteralNode;
