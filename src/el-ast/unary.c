ca526b92a61fea228eeb7c58aeb04571110e0165
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-20 15:34:52 +0200

    feat(ast/unary): add functions to format unary operator to string

--- src/el-ast/unary.c
+++ src/el-ast/unary.c
@@ -1,6 +1,21 @@
 #include <el-ast/unary.h>
 #include <el-ast/node.h>
 
+ElStringView el_ast_unary_op_to_string(ElAstUnaryExprType type) {
+    switch (type) {
+    case EL_AST_UNARY_EXPR_POS: return EL_SV("+");
+    case EL_AST_UNARY_EXPR_NEG: return EL_SV("-");
+
+    case EL_AST_UNARY_EXPR_NOT:    return EL_SV("!");
+    case EL_AST_UNARY_EXPR_BW_NOT: return EL_SV("~");
+
+    case EL_AST_UNARY_EXPR_PRE_INC:  return EL_SV("pre  ++x");
+    case EL_AST_UNARY_EXPR_PRE_DEC:  return EL_SV("pre  --x");
+    case EL_AST_UNARY_EXPR_POST_INC: return EL_SV("post x++");
+    case EL_AST_UNARY_EXPR_POST_DEC: return EL_SV("post x--");
+    }
+}
+
 ElAstNode* el_ast_new_unary_expr(ElDynArena* arena, ElAstUnaryExprType type, ElAstNode* operand) {
     ElAstNode* node = EL_DYNARENA_NEW(arena, ElAstNode);
     node->type = EL_AST_NODE_UNARY_EXPR;
