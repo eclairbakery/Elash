3ef5483ed5cdaae8ffb33878ab765d39228035b4
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-20 15:31:00 +0200

    feat(ast/bin): add functions to format binary operator to string

--- src/el-ast/bin.c
+++ src/el-ast/bin.c
@@ -1,6 +1,32 @@
 #include <el-ast/bin.h>
 #include <el-ast/node.h>
 
+ElStringView el_ast_bin_op_to_string(ElAstBinExprType type) {
+    switch (type) {
+    case EL_AST_BIN_EXPR_ADD: return EL_SV("+");
+    case EL_AST_BIN_EXPR_SUB: return EL_SV("-");
+    case EL_AST_BIN_EXPR_MUL: return EL_SV("*");
+    case EL_AST_BIN_EXPR_DIV: return EL_SV("/");
+    case EL_AST_BIN_EXPR_MOD: return EL_SV("%");
+
+    case EL_AST_BIN_EXPR_EQ : return EL_SV("==");
+    case EL_AST_BIN_EXPR_NEQ: return EL_SV("!=");
+    case EL_AST_BIN_EXPR_LT : return EL_SV("<");
+    case EL_AST_BIN_EXPR_LTE: return EL_SV("<=");
+    case EL_AST_BIN_EXPR_GT : return EL_SV(">");
+    case EL_AST_BIN_EXPR_GTE: return EL_SV(">=");
+    
+    case EL_AST_BIN_EXPR_AND: return EL_SV("&&");
+    case EL_AST_BIN_EXPR_OR : return EL_SV("||");
+    
+    case EL_AST_BIN_EXPR_BW_AND: return EL_SV("&");
+    case EL_AST_BIN_EXPR_BW_OR:  return EL_SV("|");
+    case EL_AST_BIN_EXPR_BW_XOR: return EL_SV("^");
+    case EL_AST_BIN_EXPR_SHL:    return EL_SV("<<");
+    case EL_AST_BIN_EXPR_SHR:    return EL_SV(">>");
+    }
+}
+
 ElAstNode* el_ast_new_bin_expr(ElDynArena* arena, ElAstBinExprType type, ElAstNode* left, ElAstNode* right) {
     ElAstNode* node = EL_DYNARENA_NEW(arena, ElAstNode);
     node->type = EL_AST_NODE_BINARY_EXPR;
