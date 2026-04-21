4d0a01e86d9b9312d2fa2fe896916a752d5c32b6
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-21 15:08:26 +0200

    refactor(ast): unify AST dumping functions and remove internal wrappers

--- src/el-ast/expr.c
+++ src/el-ast/expr.c
@@ -2,14 +2,14 @@
 
 #include <inttypes.h>
 
-void el_ast_dump_expr_print_ident(usize ident, FILE* out) {
-    for (usize i = 0; i < ident; ++i) {
+void el_ast_dump_print_indent(usize indent, FILE* out) {
+    for (usize i = 0; i < indent; ++i) {
         fputs("  ", out);
     }
 }
 
-void el_ast_dump_expr_literal(ElAstLiteralNode* lit, usize ident, FILE* out) {
-    el_ast_dump_expr_print_ident(ident, out);
+void el_ast_dump_expr_literal(ElAstLiteralNode* lit, usize indent, FILE* out) {
+    el_ast_dump_print_indent(indent, out);
     switch (lit->type) {
     case EL_AST_LIT_BOOL:
         fprintf(out, "BoolLiteral(%s)\n", lit->of.bool_.value ? "true" : "false");
@@ -32,37 +32,34 @@ void el_ast_dump_expr_literal(ElAstLiteralNode* lit, usize ident, FILE* out) {
     }
 }
 
-void el_ast_dump_expr_ident(ElAstIdentNode* node, usize ident, FILE* out) {
-    el_ast_dump_expr_print_ident(ident, out);
+void el_ast_dump_expr_ident(ElAstIdentNode* node, usize indent, FILE* out) {
+    el_ast_dump_print_indent(indent, out);
     fprintf(out, "IdentExpr(\""EL_SV_FMT"\")\n", EL_SV_FARG(node->name));
 }
 
-void el_ast_dump_expr_impl(ElAstExprNode* node, usize ident, FILE* out) {
+void el_ast_dump_expr(ElAstExprNode* node, usize indent, FILE* out) {
     switch (node->type) {
     case EL_AST_EXPR_BINARY: {
         ElStringView op = el_ast_bin_op_to_string(node->as.binary.type);
-        el_ast_dump_expr_print_ident(ident, out);
+        el_ast_dump_print_indent(indent, out);
         fprintf(out, "BinaryExpr('"EL_SV_FMT"'):\n", EL_SV_FARG(op));
-        el_ast_dump_expr_print_ident(ident + 1, out);
-        fprintf(out, "left: \n"); el_ast_dump_expr_impl(node->as.binary.left,  ident + 2, out);
-        el_ast_dump_expr_print_ident(ident + 1, out);
-        fprintf(out, "right: \n"); el_ast_dump_expr_impl(node->as.binary.right, ident + 2, out);
+        el_ast_dump_print_indent(indent + 1, out);
+        fprintf(out, "left: \n"); el_ast_dump_expr(node->as.binary.left,  indent + 2, out);
+        el_ast_dump_print_indent(indent + 1, out);
+        fprintf(out, "right: \n"); el_ast_dump_expr(node->as.binary.right, indent + 2, out);
         break;
     }
     case EL_AST_EXPR_UNARY: {
         ElStringView op = el_ast_unary_op_to_string(node->as.unary.type);
-        el_ast_dump_expr_print_ident(ident, out);
+        el_ast_dump_print_indent(indent, out);
         fprintf(out, "UnaryExpr('"EL_SV_FMT"'):\n", EL_SV_FARG(op));
-        el_ast_dump_expr_impl(node->as.unary.operand, ident + 1, out);
+        el_ast_dump_expr(node->as.unary.operand, indent + 1, out);
         break;
     }
     case EL_AST_EXPR_LITERAL:
-        return el_ast_dump_expr_literal(&node->as.literal, ident, out);
+        return el_ast_dump_expr_literal(&node->as.literal, indent, out);
     case EL_AST_EXPR_IDENT:
-        return el_ast_dump_expr_ident(&node->as.ident, ident, out);
+        return el_ast_dump_expr_ident(&node->as.ident, indent, out);
     }
 }
 
-void el_ast_dump_expr(ElAstExprNode* root, FILE* out) {
-    return el_ast_dump_expr_impl(root, 0, out);    
-}
