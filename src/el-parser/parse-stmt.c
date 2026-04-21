2e6e5a51232e23bb84e7975838e257e79e46f384
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-21 13:43:45 +0200

    feat(parser): implement block stmts parsing

--- src/el-parser/parse-stmt.c
+++ src/el-parser/parse-stmt.c
@@ -31,9 +31,35 @@ ElParserErrorCode _el_parser_parse_expr_stmt(ElParser* parser, ElAstStmtNode** o
     return el_parser_expect(parser, EL_TT_SEMICOLON);
 }
 
+ElParserErrorCode _el_parser_parse_block(ElParser* parser, ElAstStmtNode** out) {
+    ElAstStmtNode* head = NULL;
+    ElAstStmtNode* tail = NULL;
+
+    while (parser->current.type != EL_TT_RBRACE && parser->current.type != EL_TT_EOF) {
+        ElAstStmtNode* stmt;
+        ElParserErrorCode result = _el_parser_parse_stmt(parser, &stmt);
+        if (result != EL_PARSER_ERR_OK) {
+            return result;
+        }
+
+        el_ast_stmt_list_append(&head, &tail, stmt);
+    }
+
+    ElParserErrorCode result = el_parser_expect(parser, EL_TT_RBRACE);
+    if (result != EL_PARSER_ERR_OK) {
+        return result;
+    }
+
+    *out = el_ast_new_block_stmt(parser->arena, head);
+    return _el_parser_ret_ok(parser);
+}
+
 ElParserErrorCode _el_parser_parse_stmt(ElParser* parser, ElAstStmtNode** out) {
     if (el_parser_match(parser, EL_TT_KW_RETURN)) {
         return _el_parser_parse_return(parser, out);
+    }
+    if (el_parser_match(parser, EL_TT_LBRACE)) {
+        return _el_parser_parse_block(parser, out);
     }
     return _el_parser_parse_expr_stmt(parser, out);
 }
