70f350eb14e9bd54b9d797c08d009223c85eeee9
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-21 09:59:57 +0200

    feat(parser): add utility functions for error handling

---
+++ include/el-parser/utility.h
@@ -0,0 +1,7 @@
+#include <el-parser/parser.h>
+
+ElParserErrorCode _el_parser_ret_ok(ElParser* parser);
+ElParserErrorCode _el_parser_ret_err_impl(ElParser* parser, const ElParserErrorDetails* d);
+
+#define _el_parser_ret_err(parser, ...) \
+    _el_parser_ret_err_impl((parser), (&(const ElParserErrorDetails) { __VA_ARGS__ }))
