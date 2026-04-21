aaec4c572a193a9810cfb075757bb0c74dba8648
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-21 08:37:32 +0200

    feat(util/assert): add unreachable enum value macros

--- include/el-util/assert.h
+++ include/el-util/assert.h
@@ -18,6 +18,26 @@
     } while (0)
 #endif
 
+#if defined(__GNUC__) || defined(__clang__)
+    #define EL_UNREACHABLE_ENUM_VAL(TYPE, VAL) do { \
+        fprintf(stderr, "invalid or not handled value %d for enum %s (%s:%d)\n", \
+            (int)(VAL), #TYPE, __FILE__, __LINE__); \
+        __builtin_unreachable(); \
+    } while (0)
+#elif defined(_MSC_VER)
+    #define EL_UNREACHABLE_ENUM_VAL(TYPE, VAL) do { \
+        fprintf(stderr, "invalid or not handled value %d for enum %s (%s:%d)\n", \
+            (int)(VAL), #TYPE, __FILE__, __LINE__); \
+        __assume(0); \
+    } while (0)
+#else
+    #define EL_UNREACHABLE_ENUM_VAL(TYPE, VAL) do { \
+        fprintf(stderr, "invalid or not handled value %d for enum %s (%s:%d)\n", \
+            (int)(VAL), #TYPE, __FILE__, __LINE__); \
+        abort(); \
+    } while (0)
+#endif
+
 #ifdef EL_DEBUG
     #define EL_ASSERT(EXPR, MSG) do { \
         if (!(EXPR)) { \
