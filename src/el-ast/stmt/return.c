5ef3952b51a950506cedaef5178f9ff3d445840c
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-21 10:40:48 +0200

    feat(ast/stmt): add return stmt ast node and constructor-functions

---
+++ src/el-ast/stmt/return.c
@@ -0,0 +1,14 @@
+#include <el-ast/stmt.h>
+
+ElAstReturnStmtNode el_ast_return_stmt(ElAstExprNode* value) {
+    return (ElAstReturnStmtNode) {
+        .value = value,
+    };
+}
+
+ElAstStmtNode* el_ast_new_return_stmt(ElDynArena* arena, ElAstExprNode* value) {
+    ElAstStmtNode* node = EL_DYNARENA_NEW(arena, ElAstStmtNode);
+    node->type = EL_AST_STMT_RETURN;
+    node->as.return_ = el_ast_return_stmt(value);
+    return node;
+}
