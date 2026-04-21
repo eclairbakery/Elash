3759fe32048a348cf8f3c78e503638d2e568847f
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-21 16:02:12 +0200

    feat(parser): implement function definitions parsing

--- src/el-parser/parse-toplvl.c
+++ src/el-parser/parse-toplvl.c
@@ -6,32 +6,75 @@
 
 #include <el-util/todo.h>
 
+static ElParserErrorCode _el_parser_parse_func_params(ElParser* parser, ElAstFuncParamList* out) {
+    ElParserErrorCode result;
+    *out = el_ast_make_func_param_list();
+
+    while (parser->current.type != EL_TT_RPAREN) {
+        ElToken p_type_tok = parser->current;
+        result = el_parser_expect(parser, EL_TT_IDENT);
+        if (result != EL_PARSER_ERR_OK) return result;
+
+        ElToken p_name_tok = parser->current;
+        result = el_parser_expect(parser, EL_TT_IDENT);
+        if (result != EL_PARSER_ERR_OK) return result;
+
+        el_ast_func_param_list_append(out, el_ast_new_func_param(
+            parser->arena,
+            &el_ast_new_ident_node(parser->arena, p_type_tok.lexeme)->as.ident,
+            &el_ast_new_ident_node(parser->arena, p_name_tok.lexeme)->as.ident
+        ));
+
+        if (parser->current.type == EL_TT_COMMA) {
+            el_parser_advance(parser);
+        } else if (parser->current.type != EL_TT_RPAREN) {
+            return _el_parser_ret_err(parser, .code = EL_PARSER_ERR_EXPECTED_TOKEN, .expected = EL_TT_RPAREN);
+        }
+    }
+
+    return _el_parser_ret_ok(parser);
+}
+
 ElParserErrorCode _el_parser_parse_func_def(ElParser* parser, ElAstTopLevelNode** out) {
-    EL_TODO("not implemented");
+    ElParserErrorCode result;
+
+    ElToken ret_type_tok = parser->current;
+    result = el_parser_expect(parser, EL_TT_IDENT);
+    if (result != EL_PARSER_ERR_OK) return result;
+
+    ElToken name_tok = parser->current;
+    result = el_parser_expect(parser, EL_TT_IDENT);
+    if (result != EL_PARSER_ERR_OK) return result;
+
+    result = el_parser_expect(parser, EL_TT_LPAREN);
+    if (result != EL_PARSER_ERR_OK) return result;
+
+    ElAstFuncParamList params;
+    result = _el_parser_parse_func_params(parser, &params);
+    if (result != EL_PARSER_ERR_OK) return result;
+
+    result = el_parser_expect(parser, EL_TT_RPAREN);
+    if (result != EL_PARSER_ERR_OK) return result;
+
+    result = el_parser_expect(parser, EL_TT_LBRACE);
+    if (result != EL_PARSER_ERR_OK) return result;
+
+    ElAstStmtNode* body_stmt;
+    result = _el_parser_parse_block(parser, &body_stmt);
+    if (result != EL_PARSER_ERR_OK) return result;
+
+    *out = el_ast_new_func_definition(
+        parser->arena,
+        &el_ast_new_ident_node(parser->arena, ret_type_tok.lexeme)->as.ident,
+        &el_ast_new_ident_node(parser->arena, name_tok.lexeme)->as.ident,
+        params,
+        &body_stmt->as.block
+    );
+
+    return _el_parser_ret_ok(parser);
 }
 
 ElParserErrorCode _el_parser_parse_toplevel(ElParser* parser, ElAstTopLevelNode** out) {
-    ElAstStmtNode* stmt;
-    ElParserErrorCode result = _el_parser_parse_block(parser, &stmt);
-    if (result != EL_PARSER_ERR_OK) {
-        return result;
-    }
-
-    // TODO: stub
-    ElAstFuncParamList params = el_ast_make_func_param_list();
-    el_ast_func_param_list_append(&params, el_ast_new_func_param(
-        parser->arena,
-        &el_ast_new_ident_node(parser->arena, EL_SV("int"))->as.ident,
-        &el_ast_new_ident_node(parser->arena, EL_SV("argc"))->as.ident
-    ));
-
-    ElAstTopLevelNode* main = el_ast_new_func_definition(
-        parser->arena,
-        &el_ast_new_ident_node(parser->arena, EL_SV("int"))->as.ident,
-        &el_ast_new_ident_node(parser->arena, EL_SV("main"))->as.ident,
-        params, 
-        &stmt->as.block
-    );
-    *out = main;
-    return _el_parser_ret_ok(parser);
+    // TODO
+    return _el_parser_parse_func_def(parser, out);
 }
