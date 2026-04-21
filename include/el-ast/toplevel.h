a629e87c28b9c33bd64b662ef5d9e605dbf0c6f2
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-21 14:41:25 +0200

    feat(ast/toplvl): add function definition support to top-level nodes

--- include/el-ast/toplevel.h
+++ include/el-ast/toplevel.h
@@ -1,13 +1,15 @@
 #pragma once
 
+#include "toplevel/func.h"
+
 typedef enum ElAstTopLevelType {
-    // TODO: nothing for now
-    TODO,
+    EL_AST_TOPLVL_FUNC_DEF,
 } ElAstTopLevelType;
 
 typedef struct ElAstTopLevelNode {
     ElAstTopLevelType type;
     union {
-        // TODO: nothing for now
+        ElAstFuncDefinition func_def;
     } as;
+    ElAstTopLevelNode* next;
 } ElAstTopLevelNode;
