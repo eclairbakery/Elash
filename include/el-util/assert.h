025b3e3df12094a51b2bef54edefee5be396b008
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-21 08:37:25 +0200

    feat(util/assert): add unreachable and assert macros

---
+++ include/el-util/assert.h
@@ -0,0 +1,31 @@
+#include <stdio.h>  // NOLINT(misc-include-cleaner)
+#include <stdlib.h> // NOLINT(misc-include-cleaner)
+
+#if defined(__GNUC__) || defined(__clang__)
+    #define EL_UNREACHABLE(STR) do { \
+        fprintf(stderr, "REACHED UNREACHABLE: %s (%s:%d)\n", STR, __FILE__, __LINE__); \
+        __builtin_unreachable(); \
+    } while (0)
+#elif defined(_MSC_VER)
+    #define EL_UNREACHABLE(STR) do { \
+        fprintf(stderr, "REACHED UNREACHABLE: %s (%s:%d)\n", STR, __FILE__, __LINE__); \
+        __assume(0); \
+    } while (0)
+#else
+    #define EL_UNREACHABLE(STR) do { \
+        fprintf(stderr, "REACHED UNREACHABLE: %s (%s:%d)\n", STR, __FILE__, __LINE__); \
+        abort(); \
+    } while (0)
+#endif
+
+#ifdef EL_DEBUG
+    #define EL_ASSERT(EXPR, MSG) do { \
+        if (!(EXPR)) { \
+            fprintf(stderr, "ASSERTION FAILED: %s\nMessage: %s\nLocation: %s:%d\n", \
+                #EXPR, MSG, __FILE__, __LINE__); \
+            abort(); \
+        } \
+    } while (0)
+#else
+    #define EL_ASSERT(EXPR, MSG) ((void)0)
+#endif
