d634202fe862162461169a2718ced46db55f302b
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-22 17:11:47 +0200

    feat(hir): add type symbol

--- include/el-hir/symbol.h
+++ include/el-hir/symbol.h
@@ -2,10 +2,12 @@
 
 #include "symbol/var.h"
 #include "symbol/func.h"
+#include "symbol/type.h"
 
 typedef enum ElHirSymbolKind {
     EL_HIR_SYM_VAR,
     EL_HIR_SYM_FUNC,
+    EL_HIR_SYM_TYPE,
 } ElHirSymbolKind;
 
 typedef struct ElHirSymbol {
@@ -13,5 +15,6 @@ typedef struct ElHirSymbol {
     union {
         ElHirVarSymbol var;
         ElHirFuncSymbol func;
+        ElHirTypeSymbol type;
     } as;
 } ElHirSymbol;
