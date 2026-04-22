5d1246232798b4540ef0fd866d169e5f735bb35b
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-22 18:43:06 +0200

    feat(hir/tree): add basic expr nodes definitions

---
+++ include/el-hir/tree/expr.h
@@ -0,0 +1,41 @@
+#include <el-sema/type.h>
+
+// TODO: this should be moved from ast to sema module, probably
+#include <el-ast/expr/literal.h>
+
+#include <el-sema/expr/bin-op.h>
+#include <el-sema/expr/unary-op.h>
+
+#include <el-hir/symbol.h>
+
+typedef struct ElHirExprNode ElHirExprNode;
+
+typedef enum ElHirExprKind {
+    EL_HIR_EXPR_BINARY,
+    EL_HIR_EXPR_UNARY,
+    EL_HIR_EXPR_LITERAL,
+    EL_HIR_EXPR_SYMBOL, // resolved identifier
+} ElHirExprKind;
+
+typedef struct ElHirBinExprNode {
+    ElHirExprNode* left;
+    ElSemaBinOp op;
+    ElHirExprNode* right;
+} ElHirBinExprNode;
+
+typedef struct ElHirUnaryExprNode {
+    ElSemaUnaryOp op;
+    ElHirExprNode* operand;
+} ElHirUnaryExprNode;
+
+struct ElHirExprNode {
+    ElHirExprKind kind;
+    ElType* type;
+    union {
+        ElHirBinExprNode binary;
+        ElHirUnaryExprNode unary;
+        ElAstLiteralNode literal;
+        ElHirSymbol* symbol;
+    } as;
+};
+
