e389be688945a68d289a9ef2772e8e2b9a6e6b33
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-22 18:13:34 +0200

    refactor(sema): rename unary expr type to unary op

--- src/el-ast/expr/unary.c
+++ src/el-ast/expr/unary.c
@@ -3,14 +3,14 @@
 
 #include <el-util/assert.h>
 
-ElAstUnaryExprNode el_ast_unary_expr(ElAstUnaryExprType type, ElAstExprNode* operand) {
+ElAstUnaryExprNode el_ast_unary_expr(ElAstUnaryOp type, ElAstExprNode* operand) {
     return (ElAstUnaryExprNode) {
         .type = type,
         .operand = operand,
     };
 }
 
-ElAstExprNode* el_ast_new_unary_expr(ElDynArena* arena, ElAstUnaryExprType type, ElAstExprNode* operand) {
+ElAstExprNode* el_ast_new_unary_expr(ElDynArena* arena, ElAstUnaryOp type, ElAstExprNode* operand) {
     ElAstExprNode* node = EL_DYNARENA_NEW(arena, ElAstExprNode);
     node->type = EL_AST_EXPR_UNARY;
     node->as.unary = el_ast_unary_expr(type, operand);
