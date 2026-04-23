6d7efa91690b76b7a8cce59b86f3b183413572c5
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-23 17:12:20 +0200

    feat(hir/symbol): store function parameters as symbols

--- src/el-hir/dump/toplevel.c
+++ src/el-hir/dump/toplevel.c
@@ -1,7 +1,10 @@
 #include <el-hir/dump/toplevel.h>
 #include <el-hir/dump/stmt.h>
 #include <el-hir/dump/indent.h>
+#include <el-hir/dump/symbol.h>
+
 #include <el-hir/tree/toplevel.h>
+
 #include <el-sema/type.h>
 #include <el-defs/sv.h>
 
@@ -12,16 +15,21 @@ void el_hir_dump_toplevel(ElHirTopLevelNode* node, usize indent, FILE* out) {
         switch (curr->kind) {
         case EL_HIR_TOPLVL_FUNC_DEF: {
             ElHirFuncDefinition* func = &curr->as.func_def; 
+            ElHirFuncSymbol* sym = &func->symbol->as.func;
 
             el_hir_dump_print_indent(indent, out);
             fputs("define ", out);
-            el_sema_dump_type(func->symbol->as.func.ret_type, out);
+            el_sema_dump_type(sym->ret_type, out);
             fputc(' ', out);
             fprintf(out, EL_SV_FMT "(", EL_SV_FARG(func->symbol->name));
             
-            for (usize i = 0; i < func->symbol->as.func.param_count; ++i) {
+            for (usize i = 0; i < sym->param_count; ++i) {
+                ElHirSymbol* param = sym->params[i];
                 if (i > 0) fputs(", ", out);
-                el_sema_dump_type(func->symbol->as.func.param_types[i], out);
+
+                el_sema_dump_type(param->as.var.type, out);
+                fputc(' ', out);
+                el_hir_dump_symbol(param, out);
             }
             fputs(")", out);
             
