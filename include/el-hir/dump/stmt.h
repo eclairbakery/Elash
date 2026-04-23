3173bc0a461264126b1b95b232ac958cbb48245c
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-23 16:04:02 +0200

    feat(hir/dump): add dump stmt function

---
+++ include/el-hir/dump/stmt.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <el-defs/inttypes.h>
+#include <stdio.h>
+
+typedef struct ElHirStmtNode ElHirStmtNode;
+
+void el_hir_dump_stmt(ElHirStmtNode* node, usize indent, FILE* out);
---
+++ src/el-hir/dump/stmt.c
@@ -0,0 +1,35 @@
+#include <el-hir/dump/stmt.h>
+#include <el-hir/dump/expr.h>
+#include <el-hir/dump/indent.h>
+#include <el-hir/tree/stmt.h>
+
+void el_hir_dump_stmt(ElHirStmtNode* node, usize indent, FILE* out) {
+    if (!node) return;
+
+    for (ElHirStmtNode* curr = node; curr; curr = curr->next) {
+        switch (curr->kind) {
+        case EL_HIR_STMT_EXPR:
+            el_hir_dump_expr(curr->as.expr, indent, out);
+            fputs(";\n", out);
+            break;
+
+        case EL_HIR_STMT_RETURN:
+            el_hir_dump_print_indent(indent, out);
+            fputs("return", out);
+            if (curr->as.return_.value) {
+                fputs(" ", out);
+                el_hir_dump_expr(curr->as.return_.value, 0, out);
+            }
+            fputs(";\n", out);
+            break;
+
+        case EL_HIR_STMT_BLOCK:
+            el_hir_dump_print_indent(indent, out);
+            fputs("{\n", out);
+            el_hir_dump_stmt(curr->as.block.stmts, indent + 1, out);
+            el_hir_dump_print_indent(indent, out);
+            fputs("}\n", out);
+            break;
+        }
+    }
+}
