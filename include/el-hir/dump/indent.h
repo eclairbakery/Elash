97bae8a2ac35c5318b869f9efde0010a9f852525
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-23 14:53:43 +0200

    feat(hir/dump): add print indent function

---
+++ include/el-hir/dump/indent.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <el-defs/inttypes.h>
+
+#include <stdio.h>
+
+void el_hir_dump_print_indent(usize ident, FILE* out);
---
+++ src/el-hir/dump/indent.c
@@ -0,0 +1,7 @@
+#include <el-hir/dump/indent.h>
+
+void el_ast_dump_print_indent(usize indent, FILE* out) {
+    for (usize i = 0; i < indent; ++i) {
+        fputs("  ", out);
+    }
+}
