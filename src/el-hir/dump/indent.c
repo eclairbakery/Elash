a0847af568b43b7a9618cddbdb04360ed3e5f7ff
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-23 15:15:50 +0200

    fix(hir/dump/indent): fix function name (copy-paste moment)

--- src/el-hir/dump/indent.c
+++ src/el-hir/dump/indent.c
@@ -1,6 +1,6 @@
 #include <el-hir/dump/indent.h>
 
-void el_ast_dump_print_indent(usize indent, FILE* out) {
+void el_hir_dump_print_indent(usize indent, FILE* out) {
     for (usize i = 0; i < indent; ++i) {
         fputs("  ", out);
     }
