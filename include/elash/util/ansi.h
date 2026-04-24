f74b7f29752e538f75aa4048139bc5ed1756de48
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-25 00:16:07 +0200

    feat(util/ansi): add ansi escape code utility

---
+++ include/elash/util/ansi.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdio.h>
+
+typedef enum ElAnsiColor {
+    EL_ANSI_CLR_BLACK          = 30,
+    EL_ANSI_CLR_RED,
+    EL_ANSI_CLR_GREEN,
+    EL_ANSI_CLR_YELLOW,
+    EL_ANSI_CLR_BLUE,
+    EL_ANSI_CLR_MAGENTA,
+    EL_ANSI_CLR_CYAN,
+    EL_ANSI_CLR_WHITE,
+    EL_ANSI_CLR_BRIGHT_BLACK   = 90,
+    EL_ANSI_CLR_BRIGHT_RED,
+    EL_ANSI_CLR_BRIGHT_GREEN,
+    EL_ANSI_CLR_BRIGHT_YELLOW,
+    EL_ANSI_CLR_BRIGHT_BLUE,
+    EL_ANSI_CLR_BRIGHT_MAGENTA,
+    EL_ANSI_CLR_BRIGHT_CYAN,
+    EL_ANSI_CLR_BRIGHT_WHITE,
+    EL_ANSI_CLR_DEFAULT        = 39,
+} ElAnsiColor;
+
+typedef enum ElAnsiDecoration {
+    EL_ANSI_DEC_NONE        = 0,
+    EL_ANSI_DEC_BOLD        = 1 << 0,
+    EL_ANSI_DEC_ITALIC      = 1 << 1,
+    EL_ANSI_DEC_UNDERLINE   = 1 << 2,
+} ElAnsiDecoration;
+
+typedef struct ElAnsiStyle {
+    ElAnsiColor bg_color;
+    ElAnsiColor fg_color;
+    ElAnsiDecoration dec;
+} ElAnsiStyle;
+
+#define EL_ANSI_STYLE_DEFAULT ((ElAnsiStyle) { \
+        .bg_color = EL_ANSI_CLR_DEFAULT,       \
+        .fg_color = EL_ANSI_CLR_DEFAULT,       \
+        .dec = EL_ANSI_DEC_NONE                \
+    })
+
+bool el_ansi_is_supported();
+void el_ansi_apply_style(ElAnsiStyle style, FILE* out);
---
+++ src/elash/util/ansi.c
@@ -0,0 +1,42 @@
+#define _POSIX_C_SOURCE 200809L // for fileno
+#include <elash/util/ansi.h>
+
+#ifdef _WIN32
+    #include <io.h>
+    #define ISATTY _isatty
+    #define FILENO _fileno
+#else
+    #include <unistd.h>
+    #define ISATTY isatty
+    #define FILENO fileno
+#endif
+
+bool el_ansi_is_supported() {
+    return ISATTY(FILENO(stdout));
+}
+
+void el_ansi_apply_style(ElAnsiStyle style, FILE* out) {
+    if (!el_ansi_is_supported()) return;
+
+    // start with reset to ensure a clean state.
+    fprintf(out, "\033[0");
+
+    if (style.dec & EL_ANSI_DEC_BOLD) {
+        fprintf(out, ";1");
+    }
+    if (style.dec & EL_ANSI_DEC_ITALIC) {
+        fprintf(out, ";3");
+    }
+    if (style.dec & EL_ANSI_DEC_UNDERLINE) {
+        fprintf(out, ";4");
+    }
+
+    if (style.fg_color != EL_ANSI_CLR_DEFAULT) {
+        fprintf(out, ";%d", (int)style.fg_color);
+    }
+    if (style.bg_color != EL_ANSI_CLR_DEFAULT) {
+        fprintf(out, ";%d", (int)style.bg_color + 10);
+    }
+
+    fprintf(out, "m");
+}
