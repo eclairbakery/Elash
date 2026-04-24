9edd50a64c781faec9a5f6d1241d0c8c141f545c
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-24 19:39:17 +0200

    feat(hir/tree): add module node and associated functions

---
+++ src/el-hir/tree/module.c
@@ -0,0 +1,21 @@
+#include <el-hir/tree/module.h>
+#include <stddef.h>
+
+ElHirModule* el_hir_new_module(ElDynArena* arena) {
+    ElHirModule* mod = EL_DYNARENA_NEW(arena, ElHirModule);
+    mod->head = NULL;
+    mod->tail = NULL;
+    mod->count = 0;
+    return mod;
+}
+
+void el_hir_module_append(ElHirModule* mod, ElHirTopLevelNode* node) {
+    if (mod->head == NULL) {
+        mod->head = node;
+        mod->tail = node;
+    } else {
+        mod->tail->next = node;
+        mod->tail = node;
+    }
+    mod->count++;
+}
