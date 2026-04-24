1e07950a9c008edb33c0c87343cb6920b84d23ae
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-25 00:47:55 +0200

    feat(diag): add diagnostic meta defs

---
+++ include/elash/diag/meta.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <elash/diag/severity.h>
+
+typedef enum ElDiagMetaType {
+    EL_DIAG_META_STRING,
+    EL_DIAG_META_INTEGER,
+} ElDiagMetaType;
+
+typedef struct ElDiagMetaEntry {
+    const char* key;
+    ElDiagMetaType type;
+    union {
+        ElStringView string;
+        int integer;
+    } as;
+} ElDiagMetaEntry;
+
+#define EL_DIAG_STRING(KEY, STR) \
+    ((ElDiagMetaEntry) { .key = KEY, .type = EL_DIAG_META_STRING, .as.string = STR })
+
+#define EL_DIAG_INT(KEY, INT) \
+    ((ElDiagMetaEntry) { .key = KEY, .type = EL_DIAG_META_INTEGER, .as.integer = INT })
+
+typedef struct ElDiagMeta {
+    const ElDiagMetaEntry* entries;
+    usize count;
+} ElDiagMeta;
+
+#define EL_DIAG_META(...) ((ElDiagMeta) {                                                     \
+        .entries = (const ElDiagMetaEntry[]) { __VA_ARGS__ },                                 \
+        .count = sizeof((const ElDiagMetaEntry[]) { __VA_ARGS__ }) / sizeof(ElDiagMetaEntry), \
+    })
+
