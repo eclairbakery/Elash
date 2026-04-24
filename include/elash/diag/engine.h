5048424b4c1d74677e2bad4768dfbac0e7a5bb2e
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-25 01:03:21 +0200

    feat(diag/engine): basic diagnostics engine implementation

---
+++ include/elash/diag/engine.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <elash/util/dynarena.h>
+
+#include <elash/srcdoc/span.h>
+
+#include <elash/diag/severity.h>
+#include <elash/diag/meta.h>
+
+typedef struct ElDiagnostic {
+    ElDiagSeverity sev;
+    ElSourceSpan span;
+    ElStringView template;
+    ElDiagMeta meta;
+    
+    struct ElDiagnostic* prev;
+    struct ElDiagnostic* next;
+} ElDiagnostic;
+
+typedef struct ElDiagEngine {
+    ElDynArena* arena;
+    ElDiagnostic* diag_head;
+    ElDiagnostic* diag_tail;
+    usize diag_count;
+} ElDiagEngine;
+
+void el_diag_report_impl(
+    ElDiagEngine* engine,
+    ElDiagSeverity sev, ElSourceSpan span,
+    ElStringView template, ElDiagMeta meta
+);
+
+#define el_diag_report(engine, sev, span, template, ...) \
+    el_diag_report_impl(engine, sev, span, EL_SV(template), EL_DIAG_META(__VA_ARGS__))
---
+++ src/elash/diag/engine.c
@@ -0,0 +1,45 @@
+#include <elash/diag/engine.h>
+
+static ElDiagMeta _el_diag_clone_meta(ElDynArena* arena, ElDiagMeta meta) {
+    if (meta.count == 0) {
+        return (ElDiagMeta) { .entries = NULL, .count = 0 };
+    }
+
+    ElDiagMetaEntry* entries = EL_DYNARENA_NEW_ARR(arena, ElDiagMetaEntry, meta.count);
+    for (usize i = 0; i < meta.count; i++) {
+        entries[i].key = meta.entries[i].key;
+        entries[i].type = meta.entries[i].type;
+
+        if (entries[i].type == EL_DIAG_META_STRING) {
+            entries[i].as.string = el_dynarena_clone_sv(arena, meta.entries[i].as.string);
+        } else {
+            entries[i].as.integer = meta.entries[i].as.integer;
+        }
+    }
+
+    return (ElDiagMeta) { .entries = entries, .count = meta.count };
+}
+
+void el_diag_report_impl(
+    ElDiagEngine* engine,
+    ElDiagSeverity sev, ElSourceSpan span,
+    ElStringView template, ElDiagMeta meta
+) {
+    ElDiagnostic* diag = EL_DYNARENA_NEW(engine->arena, ElDiagnostic);
+    diag->sev = sev;
+    diag->span = span;
+    diag->template = el_dynarena_clone_sv(engine->arena, template);
+    diag->meta = _el_diag_clone_meta(engine->arena, meta);
+
+    diag->next = NULL;
+    diag->prev = engine->diag_tail;
+
+    if (engine->diag_tail) {
+        engine->diag_tail->next = diag;
+    } else {
+        engine->diag_head = diag;
+    }
+
+    engine->diag_tail = diag;
+    engine->diag_count++;
+}
