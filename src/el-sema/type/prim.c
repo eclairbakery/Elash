3821ff92c1b75057625050693974a845ca69d9a2
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-24 19:22:55 +0200

    feat(sema/type): add constructor-functions for pointer and primitive types

---
+++ src/el-sema/type/prim.c
@@ -0,0 +1,9 @@
+#include <el-sema/type/prim.h>
+#include <el-sema/type.h>
+
+ElType* el_sema_new_prim_type(ElDynArena* arena, ElPrimitiveTypeKind kind) {
+    ElType* type = EL_DYNARENA_NEW(arena, ElType);
+    type->kind = EL_TYPE_PRIM;
+    type->as.prim.kind = kind;
+    return type;
+}
