4812c0baf1f60c992312dc71f004b9387b04911b
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-25 09:43:22 +0200

    chore(diag/printer): improve diagnostics printer API

--- include/elash/diag/printer.h
+++ include/elash/diag/printer.h
@@ -1,13 +1,15 @@
 #pragma once
 
+#include <stdio.h>
+
 typedef struct ElDiagnostic ElDiagnostic;
 typedef struct ElDiagSummary ElDiagSummary;
 typedef struct ElDiagPrinter ElDiagPrinter;
 
-typedef void ElDiagPrinterBegin  (ElDiagPrinter* self);
-typedef void ElDiagPrinterPrint  (ElDiagPrinter* self, const ElDiagnostic* diag);
-typedef void ElDiagPrinterSummary(ElDiagPrinter* self, const ElDiagSummary* sum);
-typedef void ElDiagPrinterFinish (ElDiagPrinter* self);
+typedef void ElDiagPrinterBegin  (ElDiagPrinter* self, FILE* out);
+typedef void ElDiagPrinterPrint  (ElDiagPrinter* self, FILE* out, const ElDiagnostic* diag);
+typedef void ElDiagPrinterSummary(ElDiagPrinter* self, FILE* out, const ElDiagSummary* sum);
+typedef void ElDiagPrinterFinish (ElDiagPrinter* self, FILE* out);
 
 struct ElDiagPrinter {
     ElDiagPrinterBegin*   begin;
