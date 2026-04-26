705eecaed6ee011bf5a471837cf3cf2ce9e6e405
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-26 19:08:47 +0200

    feat(hir/symbol): add constructor-functions for all HIR symbol types

--- include/elash/hir/symbol/type.h
+++ include/elash/hir/symbol/type.h
@@ -1,9 +1,13 @@
 #pragma once
 
 #include <elash/sema/type.h>
+#include <elash/util/dynarena.h>
+#include <elash/defs/sv.h>
 
 typedef struct ElHirSymbol ElHirSymbol;
 
 typedef struct ElHirTypeSymbol {
     ElType* type;
 } ElHirTypeSymbol;
+
+ElHirSymbol* el_hir_new_type_symbol(ElDynArena* arena, ElStringView name, ElType* type);
--- include/elash/hir/symbol/var.h
+++ include/elash/hir/symbol/var.h
@@ -1,9 +1,13 @@
 #pragma once
 
 #include <elash/sema/type.h>
+#include <elash/util/dynarena.h>
+#include <elash/defs/sv.h>
 
 typedef struct ElHirSymbol ElHirSymbol;
 
 typedef struct ElHirVarSymbol {
     ElType* type;
 } ElHirVarSymbol;
+
+ElHirSymbol* el_hir_new_var_symbol(ElDynArena* arena, ElStringView name, ElType* type);
