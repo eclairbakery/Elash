84494a2f070878490878ad0299a05a4c68b180bd
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-30 08:44:44 +0200

    improve(defs/inttypes): remove useless typedefs

--- include/elash/defs/source-loc.h
+++ include/elash/defs/source-loc.h
@@ -3,8 +3,8 @@
 #include <elash/defs/inttypes.h>
 
 typedef struct ElSourceLocation {
-    u32 line, column;
-    u32 offset;
+    uint line, column;
+    uint offset;
 } ElSourceLocation;
 
 #define EL_SOURCE_LOC_ZERO ((ElSourceLocation) { 0 })
