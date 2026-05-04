41261859a433beabfa51a8626d2350ac221df515
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-05-04 18:20:35 +0200

    feat(hir/expr): add function call expression

--- include/elash/hir/tree/expr.h
+++ include/elash/hir/tree/expr.h
@@ -7,12 +7,14 @@
 #include "expr/bin.h"
 #include "expr/unary.h"
 #include "expr/literal.h"
+#include "expr/call.h"
 
 typedef enum ElHirExprKind {
     EL_HIR_EXPR_BINARY,
     EL_HIR_EXPR_UNARY,
     EL_HIR_EXPR_LITERAL,
     EL_HIR_EXPR_SYMBOL, // resolved identifier
+    EL_HIR_EXPR_CALL,
 } ElHirExprKind;
 
 typedef struct ElHirExprNode {
@@ -23,6 +25,7 @@ typedef struct ElHirExprNode {
         ElHirUnaryExprNode unary;
         ElHirLiteral literal;
         ElHirSymbol* symbol;
+        ElHirCallExprNode call;
     } as;
 } ElHirExprNode;
 
