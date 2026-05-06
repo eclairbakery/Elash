9a9faf4d6966d08815c6da0edf4e3245de2e5b51
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-05-06 17:41:26 +0200

    feat(mir/block): add basic block structure

---
+++ src/elash/mir/block.c
@@ -0,0 +1,20 @@
+#include <elash/mir/block.h>
+#include <string.h>
+
+ElMirBlock* el_mir_new_block(ElDynArena* arena, ElMirInstr** instructions, usize instr_count) {
+    usize size = sizeof(ElMirBlock) + sizeof(ElMirInstr*) * instr_count;
+    ElMirBlock* block = el_dynarena_alloc_zeroed(arena, size, alignof(ElMirBlock));
+    if (block != NULL) {
+        block->next = NULL;
+        block->instr_count = instr_count;
+    
+        if (instructions != NULL && instr_count > 0) {
+            memcpy(block->instructions, instructions, sizeof(ElMirInstr*) * instr_count);
+        }
+    }
+    return block;
+}
+
+ElMirBlock* el_mir_new_block_from_ibuf(ElDynArena* arena, const ElMirInstrBuf* buf) {
+    return el_mir_new_block(arena, buf->items, buf->len);
+}
