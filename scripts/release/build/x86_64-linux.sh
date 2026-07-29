fc0ac71b1b8fa7c10e98d5f7b16d692fecf07ae8
Maqi-x <186316794+Maqi-x@users.noreply.github.com>
2026-07-29 17:40:58 +0200

    fix(scripts/release): paths.

--- scripts/release/build/x86_64-linux.sh
+++ scripts/release/build/x86_64-linux.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 cd "$("$(dirname "$0")"/directory.sh "x86_64-linux")" || exit
 
-TOOLCHAIN_GXX_DIR=$(dirname "$(/opt/x86_64-linux-musl-cross/bin/x86_64-linux-musl-g++ -print-file-name=libstdc++.a)")
-TOOLCHAIN_GCC_DIR=$(dirname "$(/opt/x86_64-linux-musl-cross/bin/x86_64-linux-musl-gcc -print-file-name=libc.a)")
+TOOLCHAIN="/opt/cross/x86_64-linux-musl"
+LLVMPATH="/opt/cross/llvm/x86_64-linux-musl"
+
+TOOLCHAIN_GXX_DIR=$(dirname "$($TOOLCHAIN/bin/x86_64-linux-musl-g++ -print-file-name=libstdc++.a)")
+TOOLCHAIN_GCC_DIR=$(dirname "$($TOOLCHAIN/bin/x86_64-linux-musl-gcc -print-file-name=libc.a)")
 
 MUSL_DEPS="$HOME/build/musl-libs"
+
+export CC="$TOOLCHAIN/bin/x86_64-linux-musl-gcc"
+export AR="$TOOLCHAIN/bin/x86_64-linux-musl-ar"
 
 export EXTRA_LDFLAGS="-static -L${TOOLCHAIN_GXX_DIR} -L${TOOLCHAIN_GCC_DIR} -L${MUSL_DEPS}/lib"
 export EXTRA_CFLAGS="-I${MUSL_DEPS}/include"
 
 make -C "$PROJECT_ROOT" archive -j"$(nproc)" \
     BUILD=release \
-    CC="x86_64-linux-musl-gcc" \
-    AR="x86_64-linux-musl-ar" \
-    LLVM_CONFIG="/opt/musl-llvm/bin/llvm-config" \
+    LLVM_CONFIG="$LLVMPATH/bin/llvm-config" \
     EXTRA_CFLAGS="$EXTRA_CFLAGS" \
     EXTRA_LDFLAGS="$EXTRA_LDFLAGS" \
     OBJ_ROOT_DIR="$PWD/build/obj" \
