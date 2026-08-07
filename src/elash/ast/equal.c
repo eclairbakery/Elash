84719c26b1bdce607a6655b13a22c4940f3b40d0
Maqi-x <186316794+Maqi-x@users.noreply.github.com>
2026-08-07 16:29:54 +0200

    feat(ast): add ast nodes equality functions

---
+++ src/elash/ast/equal.c
@@ -0,0 +1,274 @@
+#include <elash/ast/equal.h>
+
+bool el_ast_equal_designator(const ElAstDesignator* a, const ElAstDesignator* b);
+
+bool el_ast_equal_type(const ElAstType* a, const ElAstType* b) {
+    if (a == b) return true;
+    if (!a || !b) return false;
+    if (a->kind != b->kind) return false;
+
+    switch (a->kind) {
+    case EL_AST_TYPE_STRUCT: {
+        if (a->as.struct_.count != b->as.struct_.count) return false;
+        ElAstDecl* fa = a->as.struct_.fields;
+        ElAstDecl* fb = b->as.struct_.fields;
+        while (fa && fb) {
+            if (!el_ast_equal_decl(fa, fb)) return false;
+            fa = fa->next;
+            fb = fb->next;
+        }
+        return fa == fb;
+    }
+    case EL_AST_TYPE_TUPLE: {
+        if (a->as.tuple.count != b->as.tuple.count) return false;
+        ElAstType* ta = a->as.tuple.head;
+        ElAstType* tb = b->as.tuple.head;
+        while (ta && tb) {
+            if (!el_ast_equal_type(ta, tb)) return false;
+            ta = ta->next;
+            tb = tb->next;
+        }
+        return ta == tb;
+    }
+    case EL_AST_TYPE_NAME:
+        return el_sv_eql(a->as.name->name, b->as.name->name);
+    case EL_AST_TYPE_REF:
+        return el_ast_equal_type(a->as.ref.base, b->as.ref.base);
+    case EL_AST_TYPE_ARRAY:
+        return el_ast_equal_type(a->as.array.base, b->as.array.base) &&
+               el_ast_equal_expr(a->as.array.size, b->as.array.size);
+    case EL_AST_TYPE_SLICE:
+        return el_ast_equal_type(a->as.slice.base, b->as.slice.base) &&
+               a->as.slice.is_raw == b->as.slice.is_raw;
+    }
+    return false;
+}
+
+bool el_ast_equal_expr(const ElAstExpr* a, const ElAstExpr* b) {
+    if (a == b) return true;
+    if (!a || !b) return false;
+    if (a->type != b->type) return false;
+
+    switch (a->type) {
+    case EL_AST_EXPR_BINARY:
+        return el_ast_equal_expr(a->as.binary.left, b->as.binary.left) &&
+               a->as.binary.op == b->as.binary.op &&
+               el_ast_equal_expr(a->as.binary.right, b->as.binary.right);
+    case EL_AST_EXPR_UNARY:
+        return a->as.unary.op == b->as.unary.op &&
+               el_ast_equal_expr(a->as.unary.operand, b->as.unary.operand);
+    case EL_AST_EXPR_LITERAL:
+        if (a->as.literal.type != b->as.literal.type) return false;
+        switch (a->as.literal.type) {
+            case EL_AST_LIT_INT: return a->as.literal.of.int_.value == b->as.literal.of.int_.value;
+            case EL_AST_LIT_FLOAT: return a->as.literal.of.float_.value == b->as.literal.of.float_.value;
+            case EL_AST_LIT_CHAR: return a->as.literal.of.char_.value == b->as.literal.of.char_.value;
+            case EL_AST_LIT_STRING: return el_sv_eql(a->as.literal.of.str_.value, b->as.literal.of.str_.value);
+            case EL_AST_LIT_BOOL: return a->as.literal.of.bool_.value == b->as.literal.of.bool_.value;
+            case EL_AST_LIT_NULL: return true;
+        }
+        return false;
+    case EL_AST_EXPR_TYPEDINIT:
+        return a->as.typedinit.scls == b->as.typedinit.scls &&
+               el_ast_equal_type(a->as.typedinit.type, b->as.typedinit.type) &&
+               el_ast_equal_init(a->as.typedinit.init, b->as.typedinit.init);
+    case EL_AST_EXPR_IDENT:
+        return el_sv_eql(a->as.ident.name, b->as.ident.name);
+    case EL_AST_EXPR_CALL: {
+        if (a->as.call.arg_count != b->as.call.arg_count) return false;
+        if (!el_ast_equal_expr(a->as.call.callee, b->as.call.callee)) return false;
+        ElAstInit* aa = a->as.call.args;
+        ElAstInit* ab = b->as.call.args;
+        while (aa && ab) {
+            if (!el_ast_equal_init(aa, ab)) return false;
+            aa = aa->next;
+            ab = ab->next;
+        }
+        return aa == ab;
+    }
+    case EL_AST_EXPR_CAST:
+        return el_ast_equal_expr(a->as.cast.expr, b->as.cast.expr) &&
+               el_ast_equal_type(a->as.cast.type, b->as.cast.type);
+    case EL_AST_EXPR_MEMBER:
+        return el_ast_equal_expr(a->as.member.expr, b->as.member.expr) &&
+               el_sv_eql(a->as.member.name, b->as.member.name);
+    case EL_AST_EXPR_TMEMBER:
+        return el_ast_equal_expr(a->as.tmember.expr, b->as.tmember.expr) &&
+               a->as.tmember.index == b->as.tmember.index;
+    }
+    return false;
+}
+
+bool el_ast_equal_func_param(const ElAstFuncParam* a, const ElAstFuncParam* b) {
+    if (a == b) return true;
+    if (!a || !b) return false;
+    return el_ast_equal_type(a->type, b->type) && el_sv_eql(a->name->name, b->name->name);
+}
+
+bool el_ast_equal_func_signature(const ElAstFuncSignature* a, const ElAstFuncSignature* b) {
+    if (!el_ast_equal_type(a->ret_type, b->ret_type)) return false;
+    if (!el_sv_eql(a->name->name, b->name->name)) return false;
+    if (a->params.count != b->params.count) return false;
+    ElAstFuncParam* pa = a->params.head;
+    ElAstFuncParam* pb = b->params.head;
+    while (pa && pb) {
+        if (!el_ast_equal_func_param(pa, pb)) return false;
+        pa = pa->next;
+        pb = pb->next;
+    }
+    return pa == pb;
+}
+
+bool el_ast_equal_decl(const ElAstDecl* a, const ElAstDecl* b) {
+    if (a == b) return true;
+    if (!a || !b) return false;
+    if (a->type != b->type) return false;
+
+    switch (a->type) {
+    case EL_AST_DECL_ALIAS:
+        return el_sv_eql(a->as.alias.name, b->as.alias.name);
+    case EL_AST_DECL_TYPEDEF:
+        return el_sv_eql(a->as.typedef_.name, b->as.typedef_.name) &&
+               el_ast_equal_type(a->as.typedef_.target, b->as.typedef_.target);
+    case EL_AST_DECL_VAR_DEF:
+        return el_ast_equal_type(a->as.var_def.type, b->as.var_def.type) &&
+               el_sv_eql(a->as.var_def.name->name, b->as.var_def.name->name) &&
+               el_ast_equal_init(a->as.var_def.init, b->as.var_def.init) &&
+               a->as.var_def.is_global == b->as.var_def.is_global;
+    case EL_AST_DECL_VAR_DECL:
+        return el_ast_equal_type(a->as.var_decl.type, b->as.var_decl.type) &&
+               el_sv_eql(a->as.var_decl.name->name, b->as.var_decl.name->name);
+    case EL_AST_DECL_FUNC_DEF: {
+        if (!el_ast_equal_func_signature(&a->as.func_def.sig, &b->as.func_def.sig)) return false;
+        ElAstStmt* sa = a->as.func_def.block->stmts;
+        ElAstStmt* sb = b->as.func_def.block->stmts;
+        while (sa && sb) {
+            if (!el_ast_equal_stmt(sa, sb)) return false;
+            sa = sa->next;
+            sb = sb->next;
+        }
+        return sa == sb;
+    }
+    case EL_AST_DECL_FUNC_DECL:
+        return el_ast_equal_func_signature(&a->as.func_decl.sig, &b->as.func_decl.sig);
+    }
+    return false;
+}
+
+bool el_ast_equal_stmt(const ElAstStmt* a, const ElAstStmt* b) {
+    if (a == b) return true;
+    if (!a || !b) return false;
+    if (a->type != b->type) return false;
+
+    switch (a->type) {
+        case EL_AST_STMT_EXPR:
+            return el_ast_equal_expr(a->as.expr, b->as.expr);
+        case EL_AST_STMT_RETURN:
+            return el_ast_equal_init(a->as.return_.value, b->as.return_.value);
+        case EL_AST_STMT_DECL:
+            return el_ast_equal_decl(a->as.decl, b->as.decl);
+        case EL_AST_STMT_ASSIGN:
+            return el_ast_equal_expr(a->as.assign.target, b->as.assign.target) &&
+                   el_ast_equal_init(a->as.assign.value, b->as.assign.value);
+        case EL_AST_STMT_BLOCK: {
+            ElAstStmt* sa = a->as.block.stmts;
+            ElAstStmt* sb = b->as.block.stmts;
+            while (sa && sb) {
+                if (!el_ast_equal_stmt(sa, sb)) return false;
+                sa = sa->next;
+                sb = sb->next;
+            }
+            return sa == sb;
+        }
+        case EL_AST_STMT_COMPOUND_ASSIGN:
+            return a->as.cassign.op == b->as.cassign.op &&
+                   el_ast_equal_expr(a->as.cassign.target, b->as.cassign.target) &&
+                   el_ast_equal_init(a->as.cassign.value, b->as.cassign.value);
+        case EL_AST_STMT_IF:
+            return el_ast_equal_expr(a->as.if_.cond, b->as.if_.cond) &&
+                   el_ast_equal_stmt(a->as.if_.then, b->as.if_.then) &&
+                   el_ast_equal_stmt(a->as.if_.else_, b->as.if_.else_);
+        case EL_AST_STMT_WHILE:
+            return el_ast_equal_expr(a->as.while_.cond, b->as.while_.cond) &&
+                   el_ast_equal_stmt(a->as.while_.body, b->as.while_.body);
+        case EL_AST_STMT_BREAK:
+        case EL_AST_STMT_CONTINUE:
+            return true;
+    }
+    return false;
+}
+
+bool el_ast_equal_designator(const ElAstDesignator* a, const ElAstDesignator* b) {
+    if (a == b) return true;
+    if (!a || !b) return false;
+    if (a->kind != b->kind) return false;
+    switch (a->kind) {
+        case EL_AST_DESIGNATOR_MEMBER:
+            return el_sv_eql(a->as.member, b->as.member);
+        case EL_AST_DESIGNATOR_TMEMBER:
+            return a->as.tmember == b->as.tmember;
+        case EL_AST_DESIGNATOR_INDEX:
+            return el_ast_equal_expr(a->as.index, b->as.index);
+    }
+    return false;
+}
+
+bool el_ast_equal_init(const ElAstInit* a, const ElAstInit* b) {
+    if (a == b) return true;
+    if (!a || !b) return false;
+    if (a->kind != b->kind) return false;
+
+    switch (a->kind) {
+    case EL_AST_INIT_EXPR:
+        return el_ast_equal_expr(a->expr, b->expr);
+    case EL_AST_INIT_LIST:
+        if (a->list.count != b->list.count) return false;
+        ElAstInit* ia = a->list.head;
+        ElAstInit* ib = b->list.head;
+        while (ia && ib) {
+            if (!el_ast_equal_init(ia, ib)) return false;
+            ia = ia->next;
+            ib = ib->next;
+        }
+        return ia == ib;
+    case EL_AST_INIT_DESIG: {
+        if (a->desig.count != b->desig.count) return false;
+        ElAstDesigInitElem* ea = a->desig.head;
+        ElAstDesigInitElem* eb = b->desig.head;
+        while (ea && eb) {
+            if (ea->desig_count != eb->desig_count) return false;
+            ElAstDesignator* da = ea->head;
+            ElAstDesignator* db = eb->head;
+            while (da && db) {
+                if (!el_ast_equal_designator(da, db)) return false;
+                da = da->next;
+                db = db->next;
+            }
+            if (da != db) return false;
+            if (!el_ast_equal_init(ea->init, eb->init)) return false;
+            ea = ea->next;
+            eb = eb->next;
+        }
+        return ea == eb;
+    }
+    case EL_AST_INIT_EMPTY:
+        return true;
+    }
+    return false;
+}
+
+bool el_ast_equal_module(const ElAstModule* a, const ElAstModule* b) {
+    if (a == b) return true;
+    if (!a || !b) return false;
+    if (a->count != b->count) return false;
+
+    ElAstDecl* da = a->head;
+    ElAstDecl* db = b->head;
+    while (da && db) {
+        if (!el_ast_equal_decl(da, db)) return false;
+        da = da->next;
+        db = db->next;
+    }
+
+    return da == db;
+}
---
+++ tests/elash/ast/test-ast-equal.c
@@ -0,0 +1,117 @@
+#include <criterion/criterion.h>
+
+#include <elash/ast/equal.h>
+#include <elash/lexer/lexer.h>
+#include <elash/util/dynarena.h>
+#include <elash/parser/parser.h>
+
+static ElSourceSpan span = EL_SOURCE_SPAN_NULL;
+
+static ElDynArena arena;
+void init() { el_dynarena_init(&arena); }
+void fini() { el_dynarena_free(&arena); }
+
+TestSuite(ast_equal, .init = init, .fini = fini);
+
+// multi-line strings in C. yes. this is real.
+#define S(...) EL_SV(#__VA_ARGS__)
+
+/// --- integration test ---
+Test(ast_equal, integration_test) {
+    ElDiagEngine diag = {0};
+    ElParser parser;
+    ElLexer lexer;
+
+    ElSourceDocument doc1, doc2;
+    el_srcdoc_init_from_str(&doc1, S(
+        int add(int a, int b) {
+            return a + b;
+        }
+        int main() {
+            struct(int, int) results = {
+                add(10, 20),
+                add(100, 200),
+            };
+            return results.0;
+        }
+    ), EL_SV("<test:doc1>"));
+    el_srcdoc_init_from_str(&doc2, S(
+        int sub(int a, int b) {
+            return a - b;
+        }
+        int main() {
+            struct(int, int) results = {
+                add(10, 20),
+                add(100, 200),
+            };
+            return results.0;
+        }
+    ), EL_SV("<test:doc2>"));
+
+    el_lexer_init(&lexer, &doc1, EL_LEXER_FLAGS_DEFAULT);
+    el_parser_init(&parser, el_lexer_as_token_stream(&lexer), &diag, &arena);
+    ElAstModule* mod1 = el_parser_parse_module(&parser);
+    cr_assert(diag.summary.total_errors == 0);
+    el_parser_destroy(&parser);
+
+    el_lexer_init(&lexer, &doc2, EL_LEXER_FLAGS_DEFAULT);
+    el_parser_init(&parser, el_lexer_as_token_stream(&lexer), &diag, &arena);
+    ElAstModule* mod2 = el_parser_parse_module(&parser);
+    cr_assert(diag.summary.total_errors == 0);
+    el_parser_destroy(&parser);
+
+    cr_assert(el_ast_equal_module(mod1, mod1));
+    cr_assert(!el_ast_equal_module(mod1, mod2));
+    cr_assert(!el_ast_equal_module(mod2, mod1));
+}
+
+/// --- normal cases ---
+Test(ast_equal, literals) {
+    // int
+    cr_assert(el_ast_equal_expr(el_ast_new_int_literal(&arena, span, 0), el_ast_new_int_literal(&arena, span, 0)));
+    cr_assert(!el_ast_equal_expr(el_ast_new_int_literal(&arena, span, 0), el_ast_new_int_literal(&arena, span, 1)));
+
+    // string
+    cr_assert(el_ast_equal_expr(el_ast_new_string_literal(&arena, span, el_sv_from_cstr("a")), el_ast_new_string_literal(&arena, span, el_sv_from_cstr("a"))));
+    cr_assert(!el_ast_equal_expr(el_ast_new_string_literal(&arena, span, el_sv_from_cstr("a")), el_ast_new_string_literal(&arena, span, el_sv_from_cstr("b"))));
+}
+
+Test(ast_equal, nulls) {
+    ElAstExpr* stuff = el_ast_new_string_literal(&arena, span, EL_SV("tuff"));
+
+    cr_assert(el_ast_equal_stmt(NULL, NULL));
+    cr_assert(!el_ast_equal_expr(NULL, stuff));
+}
+
+Test(ast_equal, expressions) {
+    // (1 + 2) * 3
+    ElAstExpr* one = el_ast_new_int_literal(&arena, span, 1);
+    ElAstExpr* two = el_ast_new_int_literal(&arena, span, 2);
+    ElAstExpr* three = el_ast_new_int_literal(&arena, span, 3);
+
+    ElAstExpr* plus = el_ast_new_bin_expr(&arena, span, EL_SEMA_BIN_OP_ADD, one, two);
+    ElAstExpr* mul = el_ast_new_bin_expr(&arena, span, EL_SEMA_BIN_OP_MUL, plus, three);
+
+    ElAstExpr* plus2 = el_ast_new_bin_expr(&arena, span, EL_SEMA_BIN_OP_ADD, el_ast_new_int_literal(&arena, span, 1), el_ast_new_int_literal(&arena, span, 2));
+    ElAstExpr* mul2 = el_ast_new_bin_expr(&arena, span, EL_SEMA_BIN_OP_MUL, plus2, el_ast_new_int_literal(&arena, span, 3));
+
+    cr_assert(el_ast_equal_expr(mul, mul2));
+
+    // different op
+    ElAstExpr* mul3 = el_ast_new_bin_expr(&arena, span, EL_SEMA_BIN_OP_SUB, plus, three);
+    cr_assert(!el_ast_equal_expr(mul, mul3));
+}
+
+Test(ast_equal, declarations) {
+    ElAstType* type = el_ast_new_type_name(&arena, span, el_ast_new_ident_raw(&arena, span, el_sv_from_cstr("int")));
+    ElAstIdent* name = el_ast_new_ident_raw(&arena, span, el_sv_from_cstr("x"));
+
+    ElAstDecl* v1 = el_ast_new_var_def(&arena, span, type, name, NULL, false);
+    ElAstDecl* v2 = el_ast_new_var_def(&arena, span, type, name, NULL, false);
+
+    cr_assert(el_ast_equal_decl(v1, v2));
+
+    // different static flag
+    ElAstDecl* v3 = el_ast_new_var_def(&arena, span, type, name, NULL, true);
+    cr_assert(!el_ast_equal_decl(v1, v3));
+}
