27032256637e5a9b035ee61575351e8a371fc807
Maqi-x <186316794+Maqi-x@users.noreply.github.com>
2026-08-07 23:17:20 +0200

    feat(unparser): make it source spans independent

--- tests/elash/unparser/test-unparser.c
+++ tests/elash/unparser/test-unparser.c
@@ -8,7 +8,9 @@
 #include <elash/parser/parser.h>
 #include <elash/unparser/unparser.h>
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 #include <glob.h>
 
 static ElDynArena arena;
@@ -58,4 +60,37 @@ Test(unparser, integration_test) {
 }
 
 // --- normal cases ---
-// (maybe someday)
+// NOLINTBEGIN(readability-magic-numbers)
+Test(unparser, null_span_int_literal) {
+    ElUnparser unparser;
+    ElTokenBuf toks;
+    el_tkbuf_init(&toks);
+    el_unparser_init(&unparser, &toks, &arena);
+
+    ElAstExpr* lit = el_ast_new_int_literal(&arena, EL_SOURCE_SPAN_NULL, 42);
+
+    bool success = el_unparser_unparse_expr(&unparser, lit);
+    cr_assert(success);
+
+    cr_assert_eq(toks.data[0].type, EL_TT_INT_LITERAL);
+    cr_assert_str_eq(toks.data[0].lexeme.data, "42");
+}
+
+Test(unparser, null_span_float_literal) {
+    ElUnparser unparser;
+    ElTokenBuf toks;
+    el_tkbuf_init(&toks);
+    el_unparser_init(&unparser, &toks, &arena);
+
+    const double expected = 3.14;
+    ElAstExpr* lit = el_ast_new_float_literal(&arena, EL_SOURCE_SPAN_NULL, expected);
+
+    bool success = el_unparser_unparse_expr(&unparser, lit);
+    cr_assert(success);
+
+    double actual = atof(toks.data[0].lexeme.data);
+    cr_assert_eq(toks.data[0].type, EL_TT_FLOAT_LITERAL);
+    cr_assert(fabs(actual - expected) < 1e-9, "Expected %f, got %s", expected, toks.data[0].lexeme.data);
+}
+
+// NOLINTEND(readability-magic-numbers)
