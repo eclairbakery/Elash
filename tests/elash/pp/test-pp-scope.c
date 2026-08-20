b63ac9324215386f1bacae947f119eb10cafba40
Maqi-x <186316794+Maqi-x@users.noreply.github.com>
2026-08-20 09:13:25 +0200

    refactor(pp/scope): replace init/destroy with new/free

--- tests/elash/pp/test-pp-scope.c
+++ tests/elash/pp/test-pp-scope.c
@@ -13,8 +13,7 @@ TestSuite(pp_scope, .init = init, .fini = fini);
 // NOLINTBEGIN(readability-magic-numbers)
 
 Test(pp_scope, basic_usage) {
-    ElPpScope scope;
-    el_pp_scope_init(&scope, NULL);
+    ElPpScope* scope = el_pp_scope_new(NULL);
 
     ElPpSymbol var1 = {
         .name = EL_SV("foo"),
@@ -25,23 +24,22 @@ Test(pp_scope, basic_usage) {
     };
 
     ElStringView key = EL_SV("foo");
-    cr_assert(el_pp_scope_assign(&scope, key, &var1));
+    cr_assert(el_pp_scope_assign(scope, key, &var1));
 
-    ElPpSymbol* lookup_result = el_pp_scope_lookup(&scope, key);
+    ElPpSymbol* lookup_result = el_pp_scope_lookup(scope, key);
     cr_assert_not_null(lookup_result);
     cr_assert_eq(lookup_result, &var1);
     cr_assert_eq(lookup_result->as.var->as.int_, 42);
 
-    cr_assert(el_pp_scope_deassign(&scope, key));
-    cr_assert_null(el_pp_scope_lookup(&scope, key));
+    cr_assert(el_pp_scope_deassign(scope, key));
+    cr_assert_null(el_pp_scope_lookup(scope, key));
 
-    el_pp_scope_destroy(&scope);
+    el_pp_scope_free(scope);
 }
 
 Test(pp_scope, nested_lookup) {
-    ElPpScope parent, child;
-    el_pp_scope_init(&parent, NULL);
-    el_pp_scope_init(&child, &parent);
+    ElPpScope* parent = el_pp_scope_new(NULL);
+    ElPpScope* child = el_pp_scope_new(parent);
 
     ElPpSymbol var_parent = {
         .name = EL_SV("foo"),
@@ -52,20 +50,19 @@ Test(pp_scope, nested_lookup) {
     };
 
     ElStringView key = EL_SV("foo");
-    el_pp_scope_assign(&parent, key, &var_parent);
+    el_pp_scope_assign(parent, key, &var_parent);
 
-    ElPpSymbol* lookup = el_pp_scope_lookup(&child, key);
+    ElPpSymbol* lookup = el_pp_scope_lookup(child, key);
     cr_assert_not_null(lookup);
     cr_assert_eq(lookup, &var_parent, "child should see parent variable");
 
-    el_pp_scope_destroy(&child);
-    el_pp_scope_destroy(&parent);
+    el_pp_scope_free(child);
+    el_pp_scope_free(parent);
 }
 
 Test(pp_scope, shadowing) {
-    ElPpScope parent, child;
-    el_pp_scope_init(&parent, NULL);
-    el_pp_scope_init(&child, &parent);
+    ElPpScope* parent = el_pp_scope_new(NULL);
+    ElPpScope* child = el_pp_scope_new(parent);
 
     ElPpSymbol var_parent = {
         .name = EL_SV("foo"),
@@ -84,17 +81,17 @@ Test(pp_scope, shadowing) {
     };
 
     ElStringView key = EL_SV("foo");
-    el_pp_scope_assign(&parent, key, &var_parent);
-    el_pp_scope_assign(&child, key, &var_child);
+    el_pp_scope_assign(parent, key, &var_parent);
+    el_pp_scope_assign(child, key, &var_child);
 
-    ElPpSymbol* lookup_child = el_pp_scope_lookup(&child, key);
+    ElPpSymbol* lookup_child = el_pp_scope_lookup(child, key);
     cr_assert_eq(lookup_child, &var_child, "child should shadow parent");
 
-    ElPpSymbol* lookup_parent = el_pp_scope_lookup(&parent, key);
+    ElPpSymbol* lookup_parent = el_pp_scope_lookup(parent, key);
     cr_assert_eq(lookup_parent, &var_parent, "parent should remain unaffected by shadowing");
 
-    el_pp_scope_destroy(&child);
-    el_pp_scope_destroy(&parent);
+    el_pp_scope_free(child);
+    el_pp_scope_free(parent);
 }
 
 // NOLINTEND(readability-magic-numbers)
